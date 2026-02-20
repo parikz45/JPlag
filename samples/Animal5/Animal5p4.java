62062a0bc59487ded92fd33935f0ce8ae2845c91
anuanmariya <tve23cs040@cet.ac.in>
2026-02-20 15:20:26 +0530

    Update Animal5p4.java

--- samples/Animal5/Animal5p4.java
+++ samples/Animal5/Animal5p4.java
@@ -107,7 +107,7 @@ public abstract class Animal extends Actor
     
     protected boolean canBreed()
     {
-        return age < BREEDING_AGE;
+        return !(age < BREEDING_AGE);
     }
 
     
